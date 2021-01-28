335478f1e598db381cb0ed9611c6c43280c49a4b
dondestefano <dondestefano@users.noreply.github.com>
2021-01-28 20:19:01 +0100

    Adds tab navigator to authenticated screenstack.

--- navigation/AuthenticatedNavigationStack.js
+++ navigation/AuthenticatedNavigationStack.js
@@ -1,28 +1,21 @@
   
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import Character from '../screens/Character'
-import Combat from '../screens/Combat'
+import { SafeAreaView } from 'react-native';
+import Character from '../screens/Character';
+import Combat from '../screens/Combat';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function AuthenticatedNavigationStack() {
   return (
-    <Stack.Navigator initialRouteName="Products">
-    <Stack.Screen 
+    <Tab.Navigator initialRouteName="Products">
+    <Tab.Screen 
         name="Character" 
-        component={Character}
-        options={({ navigation }) => ({
-            headerRight: () => (
-        <Button
-            title="To Combat"
-            onPress={() => navigation.navigate('Combat')}
-        />
-        ),
-    })} />
-<Stack.Screen name="Combat" component={Combat} />
-</Stack.Navigator>
+        component={Character} />
+<Tab.Screen name="Combat" component={Combat} />
+</Tab.Navigator>
   );
 }
 
