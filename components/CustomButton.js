4b806cbcbb792785ac0dfb49d24f28ca6a05fe8c
dondestefano <dondestefano@users.noreply.github.com>
2021-02-02 21:13:52 +0100

    Adds CustomButton.

---
+++ components/CustomButton.js
@@ -0,0 +1,21 @@
+import React, { useContext, useState } from "react";
+import {
+    View,
+    Image,
+    TouchableOpacity,
+  } from "react-native";
+
+export default function CustomButton({image, onPressEffect}){
+
+    return(
+        <View>
+            <TouchableOpacity onPress={() => onPressEffect()} activeOpacity={0.8}>
+                  <Image
+                  style={{ width: 120, height: 60 }}
+                  source={image}
+                  />
+              </TouchableOpacity>
+        </View>
+    )
+
+}
